49898f938824db3248ee34396335078074bc078f
Maca <maca.esc.k@gmail.com>
2026-03-29 22:10:03 -0300

    Parte 1: Ejercicio 1

---
+++ Parte1/ejercicio1.js
@@ -0,0 +1,12 @@
+const productos = [
+{ id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },
+{ id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },
+{ id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" },
+{ id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
+{ id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
+];
+
+console.log("--- Parte 1: Acceso a elementos y recorrido de arrays ---\n");
+console.log("1. Acceder a propiedades individuales:\nMostrá en la consola el nombre del primer producto de la lista.\n");
+
+console.log(productos[0].nombre);
